c5c72e0ae0f0ed3f3c16e4eacbb6a41b1f82b327
Rheza Budiono <rheza@uchicago.edu>
2018-01-26 18:52:32 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Sentinews
+
+Using [NewsAPI](https://newsapi.org/), [Newspaper](https://github.com/codelucas/newspaper), and [FastText](https://github.com/facebookresearch/fastText/tree/master/python) to create a function avg_sentiment that takes a query (e.g. Facebook) and optional parameters and returns an average sentiment and Pandas DataFrame including cleaned text and their corresponding sentiment labels.
+
+To use, clone FastText within the project directory and install using pip3 following directions in the linked FastText repo. 
---
+++ sentinews.py
@@ -0,0 +1,76 @@
+import newspaper
+from newspaper import Article
+import requests
+from newsapi import NewsApiClient
+import fastText
+import re
+from nltk.corpus import stopwords
+import pandas as pd
+
+api = NewsApiClient(api_key='e4d6830ad0ac44f79645297b4af19f9f')
+
+def get_articles(query, 
+                 sources='bloomberg,cnbc,the-economist,the-wall-street-journal,financial-times',
+                 from_date = None,
+                 to_date = None,
+                 sort_by = 'popularity'):
+    result = []
+    response = api.get_everything(q = query, sources = sources, from_parameter=from_date, 
+                                      to = to_date, sort_by = sort_by)
+    # print(response['totalResults'])
+    for a in response['articles']:
+            article = Article(a['url'])
+            article.download()
+            article.parse()
+            # article.nlp()
+            # print(article.text)
+            result.append(clean(article.text, stop=False))
+    return result
+
+def clean(text, stop=True):
+    text = re.sub("[^a-zA-Z]", " ", text)
+    text = text.lower()
+    if stop:
+        for w in stopwords.words('english'):
+            text.replace("w", "")
+    return text
+
+def compute_sentiment(texts, model):
+    senti = []
+    for t in texts:
+        score = model.predict(t)
+        senti.append(score)
+    return senti
+
+def translate_label(label, type='full'):
+    if type == 'full':
+        key = {'__label__1' : -2, 
+               '__label__2' : -1,
+               '__label__3' : 0,
+               '__label__4' : 1,
+               '__label__5' : 2}
+
+    elif type == 'polarity':
+        key = {'__label__1': -1, 
+               '__label__2': 1}
+
+    return key[label]
+
+def avg_sentiment(q, type='full', 
+                  sources='bloomberg,cnbc,the-economist,the-wall-street-journal,financial-times',
+                  from_date=None, to_date=None,
+                  sort_by = 'popularity'):
+    model = fastText.load_model("amazon_review_"+type+".ftz")
+    articles = get_articles(q,sources,from_date,to_date,sort_by)
+    sentiment = compute_sentiment(articles, model)
+    df = pd.DataFrame({'articles': articles, 'sentiment': sentiment})
+    avg = 0
+    for s in sentiment:
+        avg += translate_label(s[0][0], type=type)
+
+    # avg = 1.0*(avg / len(sentiment))
+    print(df)
+    print(avg)
+    return avg, df
+
+
